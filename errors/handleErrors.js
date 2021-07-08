0a3289d7a41ef05767011628fc1729263aa70682
Roman <mrroman6552@gmail.com>
2021-07-08 21:53:15 +0300

    refactor: handleErrors

--- errors/handleErrors.js
+++ errors/handleErrors.js
@@ -1,11 +1,12 @@
 exports.handleErrors = (err, req, res, next) => {
-  const { statusCode = 500, message } = err;
+  const { statusCode = 500, message, name } = err;
   res
     .status(statusCode)
     .send({
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
         : message,
+      name,
     });
   return next();
 };
